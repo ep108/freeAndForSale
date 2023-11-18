bd5e787a3424870ca49f534005bc559a05b6c65f
Edith Po <ep108@cs.wellesley.edu>
2023-11-17 23:49:59 -0500

    adding changes to post.html and queries.html

---
+++ queries.py
@@ -0,0 +1,12 @@
+# queries.py
+# CS304 Fall 2023
+# Edith's Branch
+# Final Project
+
+import cs304dbi as dbi
+
+def insert_item(conn,post_id, item_name, )
+
+
+if __name__ == 'main':
+    dbi.conf('ejk')
